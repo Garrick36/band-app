5669a72e6870b620b60a8c638cd99672071c5943
Garrick <garrick.r.whittle@gmail.com>
2021-01-04 14:38:35 -0500

    update schedule

---
+++ src/app/database/membersList.ts
@@ -0,0 +1,13 @@
+import { Member } from '../models/member';
+
+export const MEMBERS: Member[] = [
+    new Member('Grant Duncan',
+        ['Lead Guitar', 'Vocals']
+    ),
+    new Member('Eric Faris',
+        ['Drums', 'Vocals']
+    ),
+    new Member('Garrick Whittle',
+        ['Bass', 'Vocals']
+    )
+]
